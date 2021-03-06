493e6c731478dcc1e2e78965ac56966cb0267fe5
Kevin Hlavaty <kdh6486@rit.edu>
2021-03-06 14:46:05 -0500

    Made tabs 4 spaces

--- src/main/java/com/webcheckers/ui/GetHomeRoute.java
+++ src/main/java/com/webcheckers/ui/GetHomeRoute.java
@@ -21,56 +21,56 @@ import com.webcheckers.util.Message;
  * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
  */
 public class GetHomeRoute implements Route {
-  private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
 
-  private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
+    private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
 
-  private final TemplateEngine templateEngine;
-  private final PlayerLobby playerLobby;
+    private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
 
-  /**
-   * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
-   *
-   * @param templateEngine
-   *   the HTML template rendering engine
-   */
-  public GetHomeRoute(final PlayerLobby playerLobby, final TemplateEngine templateEngine) {
-    this.playerLobby = playerLobby;
-    this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
-    //
-    LOG.config("GetHomeRoute is initialized.");
-  }
-
-  /**
-   * Render the WebCheckers Home page.
-   *
-   * @param request
-   *   the HTTP request
-   * @param response
-   *   the HTTP response
-   *
-   * @return
-   *   the rendered HTML for the Home page
-   */
-  @Override
-  public Object handle(Request request, Response response) {
-    LOG.finer("GetHomeRoute is invoked.");
-    //
-    Map<String, Object> vm = new HashMap<>();
-    vm.put("title", "Welcome!");
-
-    // display a user message in the Home page
-    vm.put("message", WELCOME_MSG);
-
-    vm.put("numPlayers", playerLobby.getActivePlayers().size());
-
-    if(request.session().attribute("currentUser") != null) {
-      Player currentUser = request.session().attribute("currentUser");
-      vm.put("currentUser", currentUser);
-      vm.put("activePlayers", playerLobby.getActivePlayers());
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public GetHomeRoute(final PlayerLobby playerLobby, final TemplateEngine templateEngine) {
+        this.playerLobby = playerLobby;
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        //
+        LOG.config("GetHomeRoute is initialized.");
     }
 
-    // render the View
-    return templateEngine.render(new ModelAndView(vm , "home.ftl"));
-  }
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetHomeRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
+
+        // display a user message in the Home page
+        vm.put("message", WELCOME_MSG);
+
+        vm.put("numPlayers", playerLobby.getActivePlayers().size());
+
+        if(request.session().attribute("currentUser") != null) {
+            Player currentUser = request.session().attribute("currentUser");
+            vm.put("currentUser", currentUser);
+            vm.put("activePlayers", playerLobby.getActivePlayers());
+        }
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "home.ftl"));
+    }
 }
--- src/main/resources/spark/template/freemarker/home.ftl
+++ src/main/resources/spark/template/freemarker/home.ftl
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
-  <meta http-equiv="refresh" content="10">
-  <title>Web Checkers | ${title}</title>
-  <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+    <meta http-equiv="refresh" content="10">
+    <title>Web Checkers | ${title}</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 
 <body>
 <div class="page">
 
-  <h1>Web Checkers | ${title}</h1>
+    <h1>Web Checkers | ${title}</h1>
 
-  <!-- Provide a navigation bar -->
-  <#include "nav-bar.ftl" />
+    <!-- Provide a navigation bar -->
+    <#include "nav-bar.ftl" />
 
-  <div class="body">
+    <div class="body">
 
-    <!-- Provide a message to the user, if supplied. -->
-    <#include "message.ftl" />
+        <!-- Provide a message to the user, if supplied. -->
+        <#include "message.ftl" />
 
-    <!-- TODO: future content on the Home:
-            to start games,
-            spectating active games,
-            or replay archived games
-    -->
-    <p>Number of players: ${numPlayers}</p>
-    <#if activePlayers??>
-      <ol>
-        <#list activePlayers as player>
-          <#if currentUser != player>
-            <li>${player.name}</li>
-          </#if>
-        </#list>
-      </ol>
-    </#if>
+        <!-- TODO: future content on the Home:
+                to start games,
+                spectating active games,
+                or replay archived games
+        -->
+        <p>Number of players: ${numPlayers}</p>
+        <#if activePlayers??>
+            <ol>
+                <#list activePlayers as player>
+                    <#if currentUser != player>
+                        <li>${player.name}</li>
+                    </#if>
+                </#list>
+            </ol>
+        </#if>
 
-  </div>
+    </div>
 
 </div>
 </body>
