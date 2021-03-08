3624b58820a55850674b70ad2f80d73eab08a3e0
Daniel Gardner <drg5567@rit.edu>
2021-03-08 16:50:31 -0500

    Started handle method for GetGameRoute

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,8 +1,16 @@
 package com.webcheckers.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Session;
 import spark.TemplateEngine;
+import static spark.Spark.halt;
 
-public class GetGameRoute {
+public class GetGameRoute implements Route {
     static final String TITLE = "Web Checkers";
     static final String VIEW_NAME = "game.ftl";
 
@@ -10,5 +18,11 @@ public class GetGameRoute {
 
     public GetGameRoute (final TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        Map<String, Object> viewModel = new HashMap<>();
+        return templateEngine.render(new ModelAndView(viewModel, "game.ftl"));
     }
 }
