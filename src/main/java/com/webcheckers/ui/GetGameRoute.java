a84003cb47874657d06e5ac216e8080493e0ac43
Daniel Gardner <drg5567@rit.edu>
2021-03-08 13:56:05 -0500

    Added attributes and primitive constructor for GetGameRoute

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,4 +1,14 @@
 package com.webcheckers.ui;
 
+import spark.TemplateEngine;
+
 public class GetGameRoute {
+    static final String TITLE = "Web Checkers";
+    static final String VIEW_NAME = "game.ftl";
+
+    private final TemplateEngine templateEngine;
+
+    public GetGameRoute (final TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
 }
