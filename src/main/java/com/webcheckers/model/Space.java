6e9489ee7d459a692bafa1c8649d015beb068ffa
Daniel Gardner <drg5567@rit.edu>
2021-03-08 17:46:54 -0500

    Added attributes to Space, implemented Constructor and get methods

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,4 +1,23 @@
 package com.webcheckers.model;
 
 public class Space {
+    private int cellIdx;
+    private boolean isValid;
+    private Piece piece;
+
+    public Space (int cellIdx) {
+        this.cellIdx = cellIdx;
+    }
+
+    public int getCellIdx() {
+        return cellIdx;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
 }
