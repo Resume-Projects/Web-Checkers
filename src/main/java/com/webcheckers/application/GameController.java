0dc832076bef71d4094f700f5389f474718d45aa
Corey Urbanke <cju8676@rit.edu>
2021-03-10 16:33:48 -0500

    Removed redundant code in GameController

--- src/main/java/com/webcheckers/application/GameController.java
+++ src/main/java/com/webcheckers/application/GameController.java
@@ -7,19 +7,24 @@ public class GameController {
     public static void initializeBoard(Space[][] board) {
         for (int col = 0; col < board.length; col++) {
             if (col % 2 == 1) {
+                // First piece in col
                 board[0][col].setPiece(Piece.Type.SINGLE, Piece.Color.WHITE);
-                board[0][col].setState(Space.State.OCCUPIED);
+
+                // Second piece
                 board[2][col].setPiece(Piece.Type.SINGLE, Piece.Color.WHITE);
-                board[2][col].setState(Space.State.OCCUPIED);
+
+                // Third piece
                 board[6][col].setPiece(Piece.Type.SINGLE, Piece.Color.RED);
-                board[6][col].setState(Space.State.OCCUPIED);
+
             } else {
+                // First piece in col
                 board[1][col].setPiece(Piece.Type.SINGLE, Piece.Color.WHITE);
-                board[1][col].setState(Space.State.OCCUPIED);
+
+                // Second piece
                 board[5][col].setPiece(Piece.Type.SINGLE, Piece.Color.RED);
-                board[1][col].setState(Space.State.OCCUPIED);
+
+                // Third piece
                 board[7][col].setPiece(Piece.Type.SINGLE, Piece.Color.RED);
-                board[7][col].setState(Space.State.OCCUPIED);
             }
         }
     }
