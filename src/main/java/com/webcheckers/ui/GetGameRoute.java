ec50f73609afedab4c8f46509384e7e150146097
Daniel Gardner <drg5567@rit.edu>
2021-03-10 00:11:16 -0500

    Added attributes for GetGameRoute and started working on handle

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -3,6 +3,8 @@ package com.webcheckers.ui;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -15,8 +17,16 @@ import static spark.Spark.halt;
  * The UI Controller to GET the Game page.
  */
 public class GetGameRoute implements Route {
-    static final String TITLE = "Web Checkers";
+    // Values used in the view-model for rendering the game view.
+    static final String TITLE = "Checkers Game";
     static final String VIEW_NAME = "game.ftl";
+    static final String RED_PLAYER = "redPlayer";
+    static final String WHITE_PLAYER = "whitePlayer";
+    static final String ACTIVE_COLOR = "activeColor";
+    static final String BOARD = "board";
+    static final String MESSAGE = "message";
+
+    public enum playMode {PLAY, SPECTATOR, REPLAY}
 
     private final TemplateEngine templateEngine;
 
@@ -43,7 +53,16 @@ public class GetGameRoute implements Route {
      */
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        Map<String, Object> viewModel = new HashMap<>();
-        return templateEngine.render(new ModelAndView(viewModel, "game.ftl"));
+        final Session session = request.session();
+        Map<String, Object> vm = new HashMap<>();
+        Player currentPlayer = session.attribute("currentUser");
+
+        vm.put("currentUser", currentPlayer);
+        vm.put("viewMode", playMode.PLAY);
+
+        CheckersGame game = new CheckersGame(currentPlayer, null);
+
+
+        return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
     }
 }
