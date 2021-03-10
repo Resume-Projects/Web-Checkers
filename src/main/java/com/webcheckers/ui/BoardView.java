ad74b87a8786d16568208d676d214d28e815c963
Ray Lorenzo <ral3898@g.rit.edu>
2021-03-09 21:12:46 -0500

    Documentation

--- src/main/java/com/webcheckers/ui/BoardView.java
+++ src/main/java/com/webcheckers/ui/BoardView.java
@@ -2,8 +2,15 @@ package com.webcheckers.ui;
 
 import java.util.Iterator;
 
+/**
+ * The BoardView data type.
+ */
 public class BoardView implements Iterable<Row> {
 
+    /**
+     * Creates a Java Iterator of the Rows on the checkers board
+     * @return null
+     */
     @Override
     public Iterator<Row> iterator() {
         return null;
