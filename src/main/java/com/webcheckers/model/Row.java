76c77bb3bd7ce3a7f68590379cd21961d3a491cd
Daniel Gardner <drg5567@rit.edu>
2021-03-09 22:52:45 -0500

    Moved Row to model tier after creating array list of spaces. Deleted BoardView as it was unnecessary

--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -1,36 +1,46 @@
-package com.webcheckers.ui;
+package com.webcheckers.model;
 
-import com.webcheckers.model.Space;
-
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 /**
  * The Row data type.
  */
 public class Row implements Iterable<Space> {
     private int index;
+    private List<Space> spaces;
 
     /**
      * The Row data type.
+     *
      * @param index the row index
      */
     public Row(int index) {
         this.index = index;
+        spaces = new ArrayList<>();
     }
 
     /**
      * Get the index of this row within the board.
+     *
      * @return the index of this row (0-7)
      */
     public int getIndex() {
         return index;
     }
 
+    public Space getSpace(int col) {
+        return spaces.get(col);
+    }
+
     /**
      * Create a Java Iterator of the Spaces within a single row.
+     *
      * @return null
      */
     @Override
     public Iterator<Space> iterator() {
-        return null;
+        return spaces.iterator();
     }
 }
