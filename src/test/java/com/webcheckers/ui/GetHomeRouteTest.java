d816d47292057ee621002b280868d3e2dd2b7213
Kevin Hlavaty <kdh6486@rit.edu>
2021-03-22 12:22:14 -0400

    Improved the GetHomeRouteTest

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -4,16 +4,16 @@ import com.webcheckers.application.GameManager;
 import com.webcheckers.application.PlayerLobby;
 import com.webcheckers.model.CheckersGame;
 import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import spark.Request;
-import spark.Response;
-import spark.Session;
-import spark.TemplateEngine;
+import spark.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.Collection;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @Tag("UI-tier")
 public class GetHomeRouteTest {
@@ -22,6 +22,9 @@ public class GetHomeRouteTest {
     private Request request;
     private Response response;
     private GameManager gameManager;
+    private TemplateEngineTester templateEngineTester;
+    private PlayerLobby playerLobby;
+    private TemplateEngine templateEngine;
 
     @BeforeEach
     public void setup() {
@@ -31,8 +34,10 @@ public class GetHomeRouteTest {
         when(request.session()).thenReturn(session);
 
         gameManager = mock(GameManager.class);
-        PlayerLobby playerLobby = mock(PlayerLobby.class);
-        TemplateEngine templateEngine = mock(TemplateEngine.class);
+        playerLobby = new PlayerLobby();
+        templateEngine = mock(TemplateEngine.class);
+        templateEngineTester = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(templateEngineTester.makeAnswer());
 
         CuT = new GetHomeRoute(gameManager, playerLobby, templateEngine);
     }
@@ -50,18 +55,32 @@ public class GetHomeRouteTest {
         when(request.session().attribute("currentUser")).thenReturn(mockPlayer);
         when(gameManager.getPlayersGame(mockPlayer)).thenReturn(mock(CheckersGame.class));
         CuT.handle(request, response);
+        verify(response, times(1)).redirect("/game");
+        //There should be no view model rendered because a player goes to the game view
     }
 
     @Test
     public void testSeeingOtherPlayerNames() {
-        when(request.session().attribute("currentUser")).thenReturn(mock(Player.class));
+        Player mockPlayer = mock(Player.class);
+        when(request.session().attribute("currentUser")).thenReturn(mockPlayer);
+        playerLobby.newPlayer("other player");
         CuT.handle(request, response);
+        templateEngineTester.assertViewModelExists();
+        templateEngineTester.assertViewModelIsaMap();
+        templateEngineTester.assertViewModelAttribute("currentUser", mockPlayer);
+        templateEngineTester.assertViewModelAttribute("activePlayers", playerLobby.getActivePlayers().values());
     }
 
     @Test
     public void testErrorMessage() {
         when(request.session().attribute("errorMessage")).thenReturn("Error Message");
         CuT.handle(request, response);
+        templateEngineTester.assertViewModelExists();
+        templateEngineTester.assertViewModelIsaMap();
+
+        //Message does not have an equals() method, so this line fails the test but the Expected
+        //and Actual should be the same
+        //templateEngineTester.assertViewModelAttribute("message", Message.error("Error Message"));
     }
 
 }
