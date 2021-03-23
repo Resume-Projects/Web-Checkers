0870f94f52a58ccba35342294639188cf9bd054f
Daniel Gardner <drg5567@rit.edu>
2021-03-22 23:07:01 -0400

    Cleaned up SpaceTest line spaces

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -1,11 +1,9 @@
 package com.webcheckers.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 
 @Tag("Model-tier")
 public class SpaceTest {
