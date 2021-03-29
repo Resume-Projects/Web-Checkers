355550c58e1dbfb77cb854cb7d6bb07d1d756084
Daniel Gardner <drg5567@rit.edu>
2021-03-29 18:20:11 -0400

    Finished CheckersGameTest

--- src/test/java/com/webcheckers/model/CheckersGameTest.java
+++ src/test/java/com/webcheckers/model/CheckersGameTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.util.reflection.FieldSetter;
+
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("Model-Tier")
@@ -51,6 +54,12 @@ public class CheckersGameTest {
     }
 
     @Test
+    public void nullPlayerColor_test() {
+        Player player3 = new Player("player3");
+        assertNull(CuT.getPlayerColor(player3));
+    }
+
+    @Test
     public void validSimpleMove_test() {
         Position start = new Position(5, 0);
         Position end = new Position(4, 1);
@@ -59,6 +68,10 @@ public class CheckersGameTest {
         Message message = CuT.saveAttemptedMove(move);
         assertSame(message.getText(), "Valid move");
         assertSame(message.getType(), Message.Type.INFO);
+
+        Message applyMessage = CuT.applyAttemptedMoves();
+        assertSame(applyMessage.getText(), "Move applied");
+        assertSame(applyMessage.getType(), Message.Type.INFO);
     }
 
     @Test
@@ -71,6 +84,10 @@ public class CheckersGameTest {
         Message message = CuT.saveAttemptedMove(move);
         assertSame(message.getText(), "Valid move");
         assertSame(message.getType(), Message.Type.INFO);
+
+        Message applyMessage = CuT.applyAttemptedMoves();
+        assertSame(applyMessage.getText(), "Move applied");
+        assertSame(applyMessage.getType(), Message.Type.INFO);
     }
 
     @Test
@@ -99,6 +116,10 @@ public class CheckersGameTest {
         Message message = CuT.saveAttemptedMove(move);
         assertSame(message.getText(), "Valid move");
         assertSame(message.getType(), Message.Type.INFO);
+
+        Message applyMessage = CuT.applyAttemptedMoves();
+        assertSame(applyMessage.getText(), "Move applied");
+        assertSame(applyMessage.getType(), Message.Type.INFO);
     }
 
     @Test
@@ -115,17 +136,118 @@ public class CheckersGameTest {
         Message message = CuT.saveAttemptedMove(move);
         assertSame(message.getText(), "Valid move");
         assertSame(message.getType(), Message.Type.INFO);
+
+        Message applyMessage = CuT.applyAttemptedMoves();
+        assertSame(applyMessage.getText(), "Move applied");
+        assertSame(applyMessage.getType(), Message.Type.INFO);
+    }
+
+    @Test
+    public void invalidJumpMove() {
+        Position start = new Position(5, 0);
+        Position end = new Position(3, 2);
+        Move move = new Move(start, end);
+
+        Message message = CuT.saveAttemptedMove(move);
+        assertSame(message.getText(), "Move is too far");
+        assertSame(message.getType(), Message.Type.ERROR);
+    }
+
+    @Test
+    public void haveToMakeJump_test() throws NoSuchFieldException {
+        Position start = new Position(2, 1);
+        Position end = new Position(3, 0);
+        Move move = new Move(start, end);
+
+        Space[][] board = CuT.getBoard();
+        board[3][6] = new Space(6, new Piece(Piece.Type.SINGLE, Piece.Color.RED));
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("board"), board);
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("activeColor"), Piece.Color.WHITE);
+
+        Message message = CuT.saveAttemptedMove(move);
+        assertSame(message.getText(), "There is a jump you must make");
+        assertSame(message.getType(), Message.Type.ERROR);
+    }
+
+    @Test
+    public void makeKing_test() throws NoSuchFieldException {
+        Position start = new Position(2, 5);
+        Position end = new Position(0, 3);
+        Move move = new Move(start, end);
+
+        Space[][] board = CuT.getBoard();
+        board[2][5] = new Space(5, new Piece(Piece.Type.SINGLE, Piece.Color.RED));
+        board[1][4] = new Space(4, new Piece(Piece.Type.SINGLE, Piece.Color.WHITE));
+        board[0][3] = new Space(3, Space.State.OPEN);
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("board"), board);
+
+        Message message = CuT.saveAttemptedMove(move);
+        assertSame(message.getText(), "Valid move");
+        assertSame(message.getType(), Message.Type.INFO);
+    }
+
+    @Test
+    public void kingSimpleMove_test() throws NoSuchFieldException {
+        Position start = new Position(0, 5);
+        Position end = new Position(1, 6);
+        Move move = new Move(start, end);
+
+        Space[][] board = CuT.getBoard();
+        board[0][5] = new Space(5, new Piece(Piece.Type.KING, Piece.Color.RED));
+        board[1][6] = new Space(6, Space.State.OPEN);
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("board"), board);
+
+        Message message = CuT.saveAttemptedMove(move);
+        assertSame(message.getText(), "Valid move");
+        assertSame(message.getType(), Message.Type.INFO);
+    }
+
+    @Test
+    public void kingJumpMove_test() throws NoSuchFieldException {
+        Position start = new Position(0, 5);
+        Position end = new Position(2, 3);
+        Move move = new Move(start, end);
+
+        Space[][] board = CuT.getBoard();
+        board[0][5] = new Space(5, new Piece(Piece.Type.KING, Piece.Color.RED));
+        board[2][3] = new Space(4, Space.State.OPEN);
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("board"), board);
+
+        Message message = CuT.saveAttemptedMove(move);
+        assertSame(message.getText(), "Valid move");
+        assertSame(message.getType(), Message.Type.INFO);
+    }
+
+    @Test
+    public void resetMove_test() throws NoSuchFieldException {
+        Position start = new Position(0, 5);
+        Position end = new Position(2, 3);
+        Move move = new Move(start, end);
+
+        LinkedList<Move> moves = new LinkedList<>();
+        moves.add(move);
+
+        FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("movesQueue"), moves);
+        Message message = CuT.resetAttemptedMove();
+        assertSame(message.getText(), "Attempted move was removed");
+        assertSame(message.getType(), Message.Type.INFO);
     }
 
     @Test
     public void redResign_test() {
         assertTrue(CuT.resignGame(redPlayer));
+        assertSame(CuT.getWinner(), whitePlayer);
+        assertSame(CuT.getLoser(), redPlayer);
+
+        assertTrue(CuT.isResigned());
     }
 
     @Test
     public void whiteResign_test() throws NoSuchFieldException {
         FieldSetter.setField(CuT, CuT.getClass().getDeclaredField("activeColor"), Piece.Color.WHITE);
         assertTrue(CuT.resignGame(whitePlayer));
+        assertSame(CuT.getWinner(), redPlayer);
+        assertSame(CuT.getLoser(), whitePlayer);
     }
 
     @Test
