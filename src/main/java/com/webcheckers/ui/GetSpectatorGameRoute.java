93bc9123ae8ad226f4b0fe6f6424a5f495386aaf
Corey Urbanke <cju8676@rit.edu>
2021-04-13 16:15:37 -0400

    docs for new spectator routes

--- src/main/java/com/webcheckers/ui/GetSpectatorGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetSpectatorGameRoute.java
@@ -11,20 +11,43 @@ import spark.template.freemarker.FreeMarkerEngine;
 
 import java.util.HashMap;
 
+/**
+ * The UI Controller to GET the Game page for the spectator.
+ */
 public class GetSpectatorGameRoute implements Route {
+
 
     private final PlayerLobby playerLobby;
     private final GameManager gameManager;
     private final TemplateEngine templateEngine;
 
+    /**
+     * Create the Spark Route (UI Controller) to handle all {@code GET /spectator/game} HTTP requests.
+     *
+     * @param playerLobby
+     *   stores the current players
+     * @param gameManager
+     *   stores information pertaining to the given game
+     * @param templateEngine
+     *   tempalte engine to use for rendering HTML page
+     */
     public GetSpectatorGameRoute(PlayerLobby playerLobby, GameManager gameManager, TemplateEngine templateEngine) {
         this.playerLobby = playerLobby;
         this.gameManager = gameManager;
         this.templateEngine = templateEngine;
     }
 
+    /**
+     * Render the WebCheckers game page for a spectator
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     * @return
+     *   the rendered HTML for the game being spectated
+     */
     @Override
-    public Object handle(Request request, Response response) throws Exception {
+    public Object handle(Request request, Response response) {
         //request.queryParams("gameID") will give the game's ID
         HashMap<String, Object> vm = new HashMap<>();
         Player currentUser = request.session().attribute("currentUser");
