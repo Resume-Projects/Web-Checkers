c709936d7d167440a1f5ca0dbaf31220730797eb
Daniel Gardner <drg5567@rit.edu>
2021-04-16 00:11:21 -0400

    Finished GetSpectatorStopWatchingRouteTest

--- src/test/java/com/webcheckers/ui/GetSpectatorStopWatchingRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetSpectatorStopWatchingRouteTest.java
@@ -1,10 +1,40 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.application.GameManager;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GetSpectatorStopWatchingRouteTest {
+    private GetSpectatorStopWatchingRoute CuT;
+    private GameManager gameManager;
+    private Request request;
+    private Response response;
+    private Session session;
+
     @BeforeEach
     public void setup() {
+        request = mock(Request.class);
+        response = mock(Response.class);
+        gameManager = mock(GameManager.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
 
+        CuT = new GetSpectatorStopWatchingRoute(gameManager);
+    }
+
+    @Test
+    public void handle_test() {
+        Player spectator = new Player("player 1");
+        when(request.session().attribute("currentUser")).thenReturn(spectator);
+
+        assertNull(CuT.handle(request, response));
     }
 }
