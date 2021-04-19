fb6c9abb43f2dfa83ce0d2b15b8dd77fac2cdc0c
Kevin Hlavaty <kdh6486@rit.edu>
2021-04-19 13:04:12 -0400

    GetReplayGameRouteTest gets 97% coverage

--- src/test/java/com/webcheckers/ui/GetReplayGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetReplayGameRouteTest.java
@@ -6,10 +6,14 @@ import com.webcheckers.application.GameManager;
 import com.webcheckers.application.PlayerLobby;
 import com.webcheckers.model.CheckersGame;
 import com.webcheckers.model.Player;
+import com.webcheckers.model.SavedGame;
+import com.webcheckers.model.SavedMove;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.*;
 import org.junit.jupiter.api.Tag;
+
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
@@ -20,7 +24,7 @@ import static org.mockito.Mockito.*;
 @Tag("UI-tier")
 public class GetReplayGameRouteTest {
 
-    private GetGameRoute CuT; // Component under test
+    private GetReplayGameRoute CuT; // Component under test
     private Request request;
     private Response response;
     private Session session;
@@ -31,6 +35,7 @@ public class GetReplayGameRouteTest {
     private Gson gson;
     private Player redPlayer;
     private Player whitePlayer;
+    private SavedGame savedGame;
 
     @BeforeEach
     public void setup() {
@@ -38,113 +43,55 @@ public class GetReplayGameRouteTest {
         response = mock(Response.class);
         session = mock(Session.class);
         when(request.session()).thenReturn(session);
-
+        gson = new Gson();
         gameManager = mock(GameManager.class);
+        savedGame = new SavedGame(new ArrayList<>(), new Player("AAAAAA"), new Player("DASDAFSDSAD"), 5);
+        when(gameManager.getSavedGame(any())).thenReturn(savedGame);
+        when(session.attribute("currentUser")).thenReturn(new Player("REEEEEE"));
         playerLobby = mock(PlayerLobby.class);
         templateEngine = mock(TemplateEngine.class);
-        CuT = new GetGameRoute(gameManager, playerLobby, templateEngine, gson);
-
+        CuT = new GetReplayGameRoute(gameManager, gson);
     }
 
     @Test
-    public void newGame() throws Exception {
-        redPlayer = new Player("player 1");
-        whitePlayer = new Player("player 2");
-        checkersGame = new CheckersGame(redPlayer, whitePlayer, 1);
-        when(request.session().attribute("currentUser")).thenReturn(redPlayer);
-        when(gameManager.getPlayersGame(redPlayer)).thenReturn(checkersGame);
-        final TemplateEngineTester testHelper = new TemplateEngineTester();
-        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-
-        CuT.handle(request, response);
-
-        testHelper.assertViewModelExists();
-        testHelper.assertViewModelIsaMap();
-        testHelper.assertViewModelAttribute("title", "Checkers Game");
-        testHelper.assertViewModelAttribute("redPlayer", checkersGame.getRedPlayer());
-        testHelper.assertViewModelAttribute("whitePlayer", checkersGame.getWhitePlayer());
-
-        testHelper.assertViewName(GetGameRoute.VIEW_NAME);
-    }
-
-    @Test
-    public void testGameRed() throws Exception {
-        redPlayer = mock(Player.class);
-        whitePlayer = mock(Player.class);
-        checkersGame = new CheckersGame(redPlayer, whitePlayer, 1);
-        when(session.attribute("currentUser")).thenReturn(redPlayer);
-        // when(playerLobby.  thenReturn(whitePlayer);
-        when(gameManager.getPlayersGame(redPlayer)).thenReturn(checkersGame);
+    public void doMostStuffTest() {
         CuT.handle(request, response);
     }
 
     @Test
-    public void testGameWhite() throws Exception {
-        redPlayer = mock(Player.class);
-        whitePlayer = mock(Player.class);
-        checkersGame = new CheckersGame(redPlayer, whitePlayer, 1);
-        when(session.attribute("currentUser")).thenReturn(whitePlayer);
-        // when(playerLobby.getPlayer("whitePlayer")).thenReturn(whitePlayer);
-        when(gameManager.getPlayersGame(whitePlayer)).thenReturn(checkersGame);
+    public void playerWatchingStuffTest() {
+        savedGame = mock(SavedGame.class);
+        when(savedGame.getPlayerWatching()).thenReturn(new Player("AGFSDDSA"));
+        when(gameManager.getSavedGame(any())).thenReturn(savedGame);
         CuT.handle(request, response);
     }
 
     @Test
-    public void faultySession() throws Exception {
-        //Test for when neither player is in a game and a new game needs to be made
-        redPlayer = mock(Player.class);
-        whitePlayer = mock(Player.class);
-
-        when(session.attribute("currentUser")).thenReturn(redPlayer);
-        when(request.queryParams("whitePlayer")).thenReturn("white name");
-        when(playerLobby.getPlayerFromName("white name")).thenReturn(whitePlayer);
-        when(gameManager.getNewGame(redPlayer, whitePlayer)).thenReturn(mock(CheckersGame.class));
-        //when(session.attribute("errorMessage"))
+    public void noMoreMoves() {
+        savedGame = mock(SavedGame.class);
+        when(savedGame.hasNext()).thenReturn(false);
+        when(savedGame.getGame()).thenReturn(new CheckersGame(new Player("@RADS"), new Player("@EREF"), 69));
+        when(gameManager.getSavedGame(any())).thenReturn(savedGame);
         CuT.handle(request, response);
-        // assertThat(response).isEqualTo(null);
     }
 
     @Test
-    public void whitePlayerAlreadyInGame() throws Exception {
-        //If the white player is already in a game, then a new game should not be made
-        when(request.queryParams("whitePlayer")).thenReturn("name");
-        Player mockPlayer = mock(Player.class);
-        when(playerLobby.getPlayerFromName("name")).thenReturn(mockPlayer);
-        when(gameManager.getPlayersGame(mockPlayer)).thenReturn(mock(CheckersGame.class));
+    public void getPlayerWatchingIsRedPlayer() {
+        //No idea why this won't work
+        savedGame = mock(SavedGame.class);
+        when(savedGame.hasNext()).thenReturn(false);
+        Player redPlayer = new Player("asd23D");
+        when(savedGame.getGame()).thenReturn(new CheckersGame(redPlayer, redPlayer, 69));
+        when(savedGame.getPlayerWatching()).thenReturn(redPlayer);
+        when(gameManager.getSavedGame(any())).thenReturn(savedGame);
         CuT.handle(request, response);
-        verify(response, times(1)).redirect("/");
     }
 
     @Test
-    public void playerResigned_test() throws Exception{
-        redPlayer = mock(Player.class);
-        whitePlayer = mock(Player.class);
-        checkersGame = new CheckersGame(redPlayer, whitePlayer, 1);
-        gson = new Gson();
-        CuT = new GetGameRoute(gameManager, playerLobby, templateEngine, gson);
-        when(request.session().attribute("currentUser")).thenReturn(redPlayer);
-        when(gameManager.getPlayersGame(redPlayer)).thenReturn(checkersGame);
-        checkersGame.resignGame(redPlayer);
-
+    public void nullCurrentUser() {
+        when(session.attribute("currentUser")).thenReturn(null);
         CuT.handle(request, response);
-
     }
 
-    @Test
-    public void gameOver_test() throws Exception{
-        redPlayer = mock(Player.class);
-        whitePlayer = mock(Player.class);
-        checkersGame = mock(CheckersGame.class);
-        gson = new Gson();
-        CuT = new GetGameRoute(gameManager, playerLobby, templateEngine, gson);
-        when(request.session().attribute("currentUser")).thenReturn(redPlayer);
-        when(gameManager.getPlayersGame(redPlayer)).thenReturn(checkersGame);
-        when(checkersGame.gameEnded()).thenReturn(true);
-        when(checkersGame.getWinner()).thenReturn(redPlayer);
-        when(checkersGame.getLoser()).thenReturn(whitePlayer);
-
-        CuT.handle(request, response);
-
-    }
 }
 
